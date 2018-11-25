dcd9206c78ddc61f1d9927f36ea83794127742f2
Yousab <yousab.maged@gmail.com>
2018-11-25 13:28:14 +0200

    Implement Knapsack Problem

---
+++ knapSack.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yousab
+ * Date: 11/22/18
+ * Time: 06:46 PM
+ */
+
+/**
+ * Get the maximum total value in the knapsack from respected weights and its values
+ *
+ * @param $knapsackWeight
+ * @param $weights
+ * @param $values
+ * @param $numberOfItems
+ * @return int
+ */
+function knapSack($knapsackWeight, $weights, $values, $numberOfItems) {
+
+    //Recursive stoping base case
+    if($numberOfItems == 0 || $knapsackWeight == 0) {
+        return 0;
+    }
+
+    //Ignore nth element if it is greater than knapsack weight
+    if($values[$numberOfItems - 1] > $knapsackWeight) {
+        knapsack($knapsackWeight, $weights, $values, $numberOfItems - 1);
+    }
+
+    return max($values[$numberOfItems - 1] + knapsack($knapsackWeight - $weights[$numberOfItems - 1], $weights, $values, $numberOfItems - 1), knapsack($knapsackWeight, $weights, $values, $numberOfItems - 1));
+}
+
+/**
+ * Driven code for testing implementation.
+ */
+$weights = array(3, 5, 6,4);
+$values = array(14, 8, 10, 12);
+$knapSackWeight = 18;
+$numberOfItems = count($values);
+echo knapSack($knapSackWeight, $weights, $values, $numberOfItems);
